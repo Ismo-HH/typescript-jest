4e1cf89450820a7725b80ef2efae04b2fd0e03a2
Ismo Harjunmaa <ismo.harjunmaa@gmail.com>
2025-04-22 16:08:42 +0300

    jest-junit reporter

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: 'reports', outputName: 'jest-junit.xml' }]
+  ],
+  collectCoverage: true,
   testPathIgnorePatterns: ['/node_modules/', '/bin/', '/build/', '/.git/'],
 };
