75f1d92277d0199dc8e847142e2c2ac5cb224722
Ismo Harjunmaa <97512551+Ismo-HH@users.noreply.github.com>
2025-11-18 11:18:30 +0200

    Pieni muutos

--- src/tests/dateFormatter.test.ts
+++ src/tests/dateFormatter.test.ts
@@ -2,23 +2,23 @@ import { describe, expect, test } from 'vitest';
 
 import { finnishDateString } from '../dateFormatter';
 
-const january1 = new Date('2024-01-01T00:00:00Z');
-const february1 = new Date('2024-02-01T00:00:00Z');
-const december31 = new Date('2023-12-31T00:00:00Z');
-const tammikuu2030 = new Date('2030-01-15T12:00:00.000Z');
+const tammikuu2030 = new Date('2030-01-01T12:00:00.000Z');
 
+/**
+* Tutustu expect-komennon käyttäminen osoitteessa https://vitest.dev/api/expect.html
+**/
 test('Tammikuun ensimmäinen', () => {
-    expect.fail('no tests written for maanantai 1. tammikuuta 2024');
+    expect.fail('no tests written for tiistai 1. tammikuuta 2030');
 });
 
 test('Helmikuun ensimmäinen', () => {
-    expect.fail('no tests written for torstai 1. helmikuuta 2024');
+    expect.fail('no tests written for perjantai 1. helmikuuta 2030');
+});
+
+test('Joulukuun 1. päivä 2030', () => {
+    expect.fail('no tests written for sunnuntai 1. joulukuuta 2030');
 });
 
 test('Joulukuun viimeinen', () => {
-    expect.fail('no tests written for sunnuntai 31. joulukuuta 2023');
-});
-
-test('Tammikuun 15. päivä 2030', () => {
-    expect.fail('no tests written for tiistai 15. tammikuuta 2030');
+    expect.fail('no tests written for tiistai 31. joulukuuta 2030');
 });
